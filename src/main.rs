c61650a5fb174d73a0926df397ed251c98057316
MohdSaquib114 <msmsaq114@gmail.com>
2024-09-25 22:35:09 +0530

    Learn variables, loop and conditional statements

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
 fn main() {
-    print ln!("Hello, world!");
+    let a = 1;
+    let b = 10;
+    for i in a..b {
+         println!("{} ",i)
+    }
+
+    let is_even = true;
+
+    if is_even {
+        println!("Number is even");
+    }else if !is_even {
+        println!("Number is odd")
+    }
+
+let greetings = String::from("Hello World");
+
+println!("{}",greetings);
+
+
+let mut is_male = false;
+let is_above_18 = true;
+if is_above_18 || is_male {
+    is_male = true;
+
 }
+
+  
+    let  x : u32 = 100;
+    let y = -32;
+    let z = 100.002;
+    println!("x: {}, y: {}, z: {}",x,y,z);
+    println!("Hello, world!");
+}
+//All  variables are immutable by default
+
+//i8
+//i16
+//i32
+//i128
+
+//u8
+//u16
+//u32
+//u128
+
+//f32
+//f64
